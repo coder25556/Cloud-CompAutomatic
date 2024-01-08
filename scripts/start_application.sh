efd6770db5f807727764aed06fbfad53e69f3987
buesv <buesven@gmx.de>
2024-01-08 18:29:42 +0100

    Start

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,5 +3,5 @@
 # Navigieren zum Verzeichnis mit der SNAPSHOT.jar-Datei
 cd /home/ec2-user/cloudauto
 
-# Starten Sie die Quarkus-Anwendung
+# Starten Sie die Quarkus-Anwendung.
 mvn quarkus:dev -Dquarkus.http.host=0.0.0.0
