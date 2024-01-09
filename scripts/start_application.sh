2499c5beb3280cc769f05d97bf97d74127f614bf
buesv <sfasf@gmx.de>
2024-01-09 14:22:22 +0100

    asfdasdf

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,11 +3,11 @@
 # Navigieren zum Verzeichnis mit der SNAPSHOT.jar-Datei
 cd /home/ec2-user/cloudauto || exit 1
 
-# Starten Sie die Quarkus-Anwendung mit einem Timeout
+# Starten Sie die Quarkus-Anwendung im Hintergrund mit einem Timeout
 # Setzen Sie den gewünschten Timeout-Wert in Sekunden
 TIMEOUT=60
 
-timeout --preserve-status $TIMEOUT mvn quarkus:dev -Dquarkus.http.host=0.0.0.0
+timeout --preserve-status $TIMEOUT mvn quarkus:dev -Dquarkus.http.host=0.0.0.0 &
 
 # Überprüfen Sie den Exit-Code von timeout
 EXIT_CODE=$?
@@ -16,7 +16,7 @@ if [ $EXIT_CODE -eq 124 ]; then
     echo "Das Skript hat einen Timeout erreicht nach $TIMEOUT Sekunden."
     # Führen Sie hier weitere Aktionen aus, wenn ein Timeout aufgetreten ist
 else
-    echo "Das Skript wurde erfolgreich beendet."
+    echo "Die Quarkus-Anwendung läuft im Hintergrund weiter."
     # Führen Sie hier Aktionen aus, wenn das Skript erfolgreich war
 fi
 
